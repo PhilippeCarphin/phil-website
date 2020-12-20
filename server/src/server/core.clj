04cc5ffe4a2a1086db5a2242612912545cdc172b
Philippe Carphin <philippe.carphin@polymtl.ca>
2020-12-20 13:38:33 -0500

    Use routes from compojure

--- server/src/server/core.clj
+++ server/src/server/core.clj
@@ -1,5 +1,13 @@
 (ns server.core
-  (:require [org.httpkit.server :as s]))
+  (:require [org.httpkit.server :as s]
+            [compojure.core :refer [routes POST GET ANY]]))
+
+(defn app []
+  (routes
+    (GET "/" [:as req]
+      {:status 200
+      :header {"Content-Type" "text/html"}
+      :body "<h1>HELLO WORLD APP</h1>"})))
 
 (defn handler [req]
   {:status 200
@@ -7,7 +15,7 @@
   :body "<h1>HELLO WORLD</h1>"})
 
 (defn create-server [] 
-  (s/run-server handler {:port 8080}))
+  (s/run-server (app) {:port 8080}))
 
 (defn stop-server [server]
   (server :timeout 100))
